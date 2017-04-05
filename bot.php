ea0067d99c1f21ef8776754f9a53c4a2dc49c659
diewd2a <diewd2a@gmail.com>
2017-04-06 00:23:25 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,6 +10,7 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $key => $event) {
+		$key_text .= $key;
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
@@ -22,7 +23,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text.'\r\n'.$key_text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -43,7 +44,7 @@ if (!is_null($events['events'])) {
 			$result = curl_exec($ch);
 			curl_close($ch);
 
-			echo $result . "\r\n";
+			echo $result . '\r\n';
 			exit();
 		}
 	}
